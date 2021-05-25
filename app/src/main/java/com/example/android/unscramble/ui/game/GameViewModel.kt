016840a850c39b17653d4815f06d34d0c3ca4c72
Marlon Aleman <mjaleman1997@gmail.com>
2021-05-25 00:29:27 -0700

    Add word scramble feature

--- app/src/main/java/com/example/android/unscramble/ui/game/GameViewModel.kt
+++ app/src/main/java/com/example/android/unscramble/ui/game/GameViewModel.kt
@@ -1,15 +1,55 @@
 package com.example.android.unscramble.ui.game
 
+import android.util.Log
 import androidx.lifecycle.ViewModel
 
 class GameViewModel: ViewModel (){
     private var score = 0
     private var _currentWordCount = 0
-    private var _currentScrambledWord = "test"
+    private lateinit var _currentScrambledWord: String
+    private var wordList: MutableList<String> = mutableListOf()
+    private lateinit var currentWord: String
 
     val currentWordCount: Int
         get() = _currentWordCount
 
     val currentScrambledWord: String
         get() = _currentScrambledWord
+
+    init {
+        Log.d("Game Fragment", "GameViewModel created!")
+        getNextWord()
+    }
+
+    override fun onCleared() {
+        super.onCleared()
+        Log.d("GameFragment", "GameViewModel destroyed!")
+    }
+
+    fun getNextWord() {
+        currentWord = allWordsList.random()
+        val tempWord = currentWord.toCharArray()
+        tempWord.shuffle()
+
+        // Ensures the tempWord is actually shuffled
+        while(tempWord.toString().equals(currentWord, false)){
+            tempWord.shuffle()
+        }
+
+        // Checks to see if word has been used before to prevent repeating words in the game
+        if (wordList.contains(currentWord)){
+            getNextWord()
+        } else {
+            _currentScrambledWord = String(tempWord)
+            ++_currentWordCount
+            wordList.add(currentWord)
+        }
+    }
+
+    fun nextWord(): Boolean {
+        return if(currentWordCount < MAX_NO_OF_WORDS) {
+            getNextWord()
+            true
+        } else false
+    }
 }
