ee6f5d2ec5ad457a4d303841525d7d8d8211777c
Marlon Aleman <mjaleman1997@gmail.com>
2021-05-24 23:56:06 -0700

    Initial commit

--- app/src/main/java/com/example/android/unscramble/ui/game/GameViewModel.kt
+++ app/src/main/java/com/example/android/unscramble/ui/game/GameViewModel.kt
@@ -2,6 +2,14 @@ package com.example.android.unscramble.ui.game
 
 import androidx.lifecycle.ViewModel
 
-class GameViewModel: ViewModel() {
+class GameViewModel: ViewModel (){
+    private var score = 0
+    private var _currentWordCount = 0
+    private var _currentScrambledWord = "test"
 
+    val currentWordCount: Int
+        get() = _currentWordCount
+
+    val currentScrambledWord: String
+        get() = _currentScrambledWord
 }
