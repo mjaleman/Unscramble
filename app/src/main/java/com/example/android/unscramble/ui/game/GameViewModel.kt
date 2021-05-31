300c877a98898e85c0e26c78ef4762848907d2a8
Marlon Aleman <mjaleman1997@gmail.com>
2021-05-31 00:25:47 -0700

    Add Alert Dialog box at end of game.

--- app/src/main/java/com/example/android/unscramble/ui/game/GameViewModel.kt
+++ app/src/main/java/com/example/android/unscramble/ui/game/GameViewModel.kt
@@ -4,11 +4,17 @@ import android.util.Log
 import androidx.lifecycle.ViewModel
 
 class GameViewModel: ViewModel (){
-    private var score = 0
+    private var _score = 0
     private var _currentWordCount = 0
     private lateinit var _currentScrambledWord: String
     private var wordList: MutableList<String> = mutableListOf()
     private lateinit var currentWord: String
+
+    /*
+     * Getters used to safely access data
+     */
+    val score: Int
+        get() = _score
 
     val currentWordCount: Int
         get() = _currentWordCount
@@ -16,6 +22,9 @@ class GameViewModel: ViewModel (){
     val currentScrambledWord: String
         get() = _currentScrambledWord
 
+    /*
+     * Sets up the first word of the game
+     */
     init {
         Log.d("Game Fragment", "GameViewModel created!")
         getNextWord()
@@ -26,6 +35,9 @@ class GameViewModel: ViewModel (){
         Log.d("GameFragment", "GameViewModel destroyed!")
     }
 
+    /*
+     * Prepares next word in the game and prevents the same word from popping up again.
+     */
     fun getNextWord() {
         currentWord = allWordsList.random()
         val tempWord = currentWord.toCharArray()
@@ -46,6 +58,10 @@ class GameViewModel: ViewModel (){
         }
     }
 
+    /*
+     * Returns true if the current word count is less than MAX_NO_OF_WORDS.
+     * Updates the next word
+     */
     fun nextWord(): Boolean {
         return if(currentWordCount < MAX_NO_OF_WORDS) {
             getNextWord()
