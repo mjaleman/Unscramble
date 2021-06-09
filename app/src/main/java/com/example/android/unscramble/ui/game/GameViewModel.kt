497ab992a53598417cfceefddc1223272ffb08d0
Marlon Aleman <mjaleman1997@gmail.com>
2021-06-08 22:19:58 -0700

    Convert currentScrambledWord into a Spannable String. Enables verbatim talkback for accessibility.

--- app/src/main/java/com/example/android/unscramble/ui/game/GameViewModel.kt
+++ app/src/main/java/com/example/android/unscramble/ui/game/GameViewModel.kt
@@ -1,8 +1,12 @@
 package com.example.android.unscramble.ui.game
 
+import android.text.Spannable
+import android.text.SpannableString
+import android.text.style.TtsSpan
 import android.util.Log
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.Transformations
 import androidx.lifecycle.ViewModel
 
 class GameViewModel: ViewModel (){
@@ -21,8 +25,21 @@ class GameViewModel: ViewModel (){
     val currentWordCount: LiveData<Int>
         get() = _currentWordCount
 
-    val currentScrambledWord: LiveData<String>
-        get() = _currentScrambledWord
+    val currentScrambledWord: LiveData<Spannable> = Transformations.map(_currentScrambledWord) {
+        if(it == null){
+            SpannableString("")
+        } else {
+            val scrambledWord = it.toString()
+            val spannable: Spannable = SpannableString(scrambledWord)
+            spannable.setSpan(
+                    TtsSpan.VerbatimBuilder(scrambledWord).build(),
+                    0,
+                    scrambledWord.length,
+                    Spannable.SPAN_INCLUSIVE_INCLUSIVE
+            )
+            spannable
+        }
+    }
 
     /*
      * Sets up the first word of the game
